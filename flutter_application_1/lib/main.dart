8a707ff499d5c5138df53c6f055fca43e2aee3cc
Soongyu Kwon <me@soongyu.me>
2023-04-09 23:28:18 +0900

    layouts

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,96 +1,120 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+// Uncomment lines 3 and 6 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  debugPaintSizeEnabled = false; // Set to true for visual layout
+  // debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const showCard = true; // Set to false to show Stack
-
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Center(child: showCard ? _buildCard() : _buildStack()),
-      ),
-    );
-  }
-
-  // #docregion Card
-  Widget _buildCard() {
-    return SizedBox(
-      height: 210,
-      child: Card(
-        child: Column(
+        body: ListView(
           children: [
-            ListTile(
-              title: const Text(
-                '1625 Main Street',
-                style: TextStyle(fontWeight: FontWeight.w500),
-              ),
-              subtitle: const Text('My City, CA 99984'),
-              leading: Icon(
-                Icons.restaurant_menu,
-                color: Colors.blue[500],
-              ),
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
             ),
-            const Divider(),
-            ListTile(
-              title: const Text(
-                '(408) 555-1212',
-                style: TextStyle(fontWeight: FontWeight.w500),
-              ),
-              leading: Icon(
-                Icons.contact_phone,
-                color: Colors.blue[500],
-              ),
-            ),
-            ListTile(
-              title: const Text('costa@example.com'),
-              leading: Icon(
-                Icons.contact_mail,
-                color: Colors.blue[500],
-              ),
-            ),
+            titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
     );
   }
-  // #enddocregion Card
 
-  // #docregion Stack
-  Widget _buildStack() {
-    return Stack(
-      alignment: const Alignment(0.6, 0.6),
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const CircleAvatar(
-          backgroundImage: AssetImage('images/pic0.jpg'),
-          radius: 100,
-        ),
+        Icon(icon, color: color),
         Container(
-          decoration: const BoxDecoration(
-            color: Colors.black45,
-          ),
-          child: const Text(
-            'Mia B',
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
             style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
             ),
           ),
         ),
       ],
     );
   }
-// #enddocregion Stack
 }
